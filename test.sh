2a576f6abd1bd14ed3e9ff42784ab30dd9280cdd
Xiong Yiliang <xiongyiliang@xunlei.com>
2013-12-05 20:22:10 +0800

    update test script

--- test.sh
+++ test.sh
@@ -7,9 +7,13 @@ ref="$1"
 ./stop_all.sh &&
 ./install_all.sh &&
 ./start_all.sh &&
+
+# run test
 timeout 600 ./run_test.sh
-# clean up
 ./stop_all.sh
+
+# save test results
+mkdir -p ../out/test/$ref/
 cp -r /home/root1/gfstest/result/* ../out/test/$ref/
-mv -v out/log ../out/test/$ref/
+mv -v ../out/log ../out/test/$ref/
 exit 0
