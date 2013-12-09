e309750754ac48d0ae049cdfa8e6fcde161f799f
Xiong Yiliang <xiongyiliang@xunlei.com>
2013-12-09 18:09:42 +0800

    clean up test log folder before running test

--- run-test.sh
+++ run-test.sh
@@ -12,11 +12,11 @@ ref="$1"
 
 mkdir -p out/test/tmp/
 mkdir -p out/test/$ref/
+rm -rf out/test/$ref/*
 touch out/test/$ref/TEST_IS_RUNNING
 
 ./test.sh $ref 2>&1
 
-rm -rf out/test/$ref/*
 mv -v out/test/tmp/* out/test/$ref/
 mv -v out/log out/test/$ref/setup_log
 
