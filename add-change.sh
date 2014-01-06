bd642bd159dc20bdc4eb175d4b361ba412892999
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-01-06 10:24:02 +0800

    fetch all the changes from gerrit

--- add-change.sh
+++ add-change.sh
@@ -7,7 +7,8 @@ commit=$2
 
 ( cd build && 
   git remote show | timeout 60 xargs git remote prune && 
-  timeout 60 git remote update )
+  timeout 60 git remote update &&
+  timeout 60 git fetch gerrit refs/changes/*:refs/remotes/gerrit/changes/* )
 
 if [ -e "out/pass/$commit" -o -e "out/fail/$commit" ]; then
     echo "'$change': weird, already built $commit!"
