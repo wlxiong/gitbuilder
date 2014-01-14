51d68c2c90d498fd69a202577d341a055ca5a6b0
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-01-14 17:43:57 +0800

    copy all binaries to folder nightly-$ref/ and then tar

--- archive-bin.sh
+++ archive-bin.sh
@@ -3,13 +3,16 @@ DIR="$(dirname $0)"
 cd "$DIR/build"
 
 ref=$1
+nightly=nightly-${ref:0:7}
+mkdir -p $nightly
 
-mkdir -p bin/
 bins="x_master/gfs_master x_shadow_master/gfs_shadow_master log_server/gfs_master_logger chunk_server/chunk_server sclient/FsShell"
 for f in $bins
 do
-    cp -vf $f bin/
+    cp -vf $f $nightly
 done
 
-cd bin/
-tar zcf ../../out/nightly/gfs-bin-${ref:0:7}.tar.gz *
+( cd $nightly &&
+  tar zcf ../../out/nightly/gfs-bin-${ref:0:7}.tar.gz * )
+
+rm -rf $nightly
