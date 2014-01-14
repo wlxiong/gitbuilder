7ac22b07a6cd67ab4fa07eb72f96da4c3f552e7f
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-01-14 10:52:15 +0800

    archive nightly gfs build

---
+++ archive-bin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DIR="$(dirname $0)"
+cd "$DIR/build"
+
+ref=$1
+
+tar zcf ../out/nightly/gfs-bin-${ref:0:8}.tar.gz x_master/gfs_master \
+    x_shadow_master/gfs_shadow_master \
+    log_server/gfs_master_logger \
+    chunk_server/chunk_server \
+    sclient/FsShell
