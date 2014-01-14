3e17401fe643a919f06a9a2e6fbb5c2c39521f20
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-01-14 17:30:20 +0800

    move all binaries to root of the archive

--- archive-bin.sh
+++ archive-bin.sh
@@ -4,8 +4,12 @@ cd "$DIR/build"
 
 ref=$1
 
-tar zcf ../out/nightly/gfs-bin-${ref:0:7}.tar.gz x_master/gfs_master \
-    x_shadow_master/gfs_shadow_master \
-    log_server/gfs_master_logger \
-    chunk_server/chunk_server \
-    sclient/FsShell
+mkdir -p bin/
+bins="x_master/gfs_master x_shadow_master/gfs_shadow_master log_server/gfs_master_logger chunk_server/chunk_server sclient/FsShell"
+for f in $bins
+do
+    mv -v $f bin/
+done
+
+cd bin/
+tar zcf ../../out/nightly/gfs-bin-${ref:0:7}.tar.gz *
