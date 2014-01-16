548c80f66078082fc8842ecacd2d6449187a41f3
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-01-16 17:39:40 +0800

    move ci scripts to gfs repos

--- autobuilder.sh
+++ autobuilder.sh
@@ -5,13 +5,7 @@ cd "$DIR"
 while [ "$(ls -A out/pending)" ]; do
     for file in out/pending/*; do
         set -m
-        ref=`basename $file`
-        ./run.sh $ref
-        # submit review
-        submitter=$(cat $file)
-        if [ $submitter == "gerrit" ]; then
-            ./submit-review.sh $ref "$astyle_msg"
-        fi
+        ./build/ci/run.sh $file
         rm $file
     done
 done
