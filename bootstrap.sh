0705d1f2b27178bdd82330c221bfb8e75f9a61cd
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-01-18 02:13:58 +0800

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,7 @@ cd "$DIR"
 gfs="ssh://autobuilder@10.10.15.20:29418/gfs"
 pystubber="git@10.10.15.19:/usr/local/repo/pystubber.git"
 
-rm -rf build/ gfs/ pystubber/
+rm -rf build/ pystubber/
 git clone --no-checkout --origin gerrit $gfs build
 ( cd build/ && git checkout -b dev gerrit/dev )
-git clone --no-checkout --origin gerrit $gfs gfs
-( cd gfs/ && git checkout -b dev gerrit/dev )
 git clone $pystubber pystubber
