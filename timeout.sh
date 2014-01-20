47240ddd81c36b57cf921c63a1c86083126b363d
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-01-20 13:18:52 +0800

    use timeout command if it exists

--- timeout.sh
+++ timeout.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-( cmdpid=$BASHPID; (sleep $1; kill $cmdpid) & exec "${@:2}" )
+timeout=`which timeout`
+if [ -f $timeout ]; then
+    $timeout $@
+else
+    ( cmdpid=$BASHPID; (sleep $1; kill $cmdpid) & exec "${@:2}" )
+    exit 0
+fi
