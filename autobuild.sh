35598ce5b3f6cb9b977d632c9c14ca3aa2eb58ec
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-01-21 16:42:47 +0800

    if cannot find ci scripts, report build failed

--- autobuild.sh
+++ autobuild.sh
@@ -7,9 +7,14 @@ while [ "$(ls -A out/pending)" ]; do
     for file in out/pending/*; do
         set -m
         ref=`basename $file`
+        # clean up build/
         ./clean-dir.sh $ref
         # run build
-        $CI_PATH/run-build.sh $ref | tee out/log
+        if [ -f $CI_PATH/run-build.sh ]; then
+            $CI_PATH/run-build.sh $ref | tee out/log
+        else
+            echo "cannot find $CI_PATH/run-build.sh" > out/fail/$ref
+        fi
         # make archive
         if [ -f out/pass/$ref ]; then
             $CI_PATH/archive-bin.sh $ref
--- autoreview.sh
+++ autoreview.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 DIR="$(dirname $0)"
 cd "$DIR"
+CI_PATH="build/ci"
 
 while [ "$(ls -A out/pending)" ]; do
     for file in out/pending/*; do
         set -m
         ref=`basename $file`
+        # clean up build/
         ./clean-dir.sh $ref
-        ./build/ci/run.sh $file
+        # run ci build and test
+        if [ -f $CI_PATH/run.sh ]; then
+            $CI_PATH/run.sh $file
+        else
+            echo "cannot find $CI_PATH/run.sh" > out/fail/$ref
+        fi
         rm $file
     done
 done
