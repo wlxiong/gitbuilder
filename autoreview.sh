18c46bfbd402a0e22e7110da7254f8d68e7563f1
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-02-11 16:27:42 +0800

    use tailon for real time log monitor

--- autoreview.sh
+++ autoreview.sh
@@ -13,10 +13,11 @@ while [ "$(ls -A out/pending)" ]; do
         # run ci build and test
         review_script=$CI_PATH/run-review.sh
         if [ -f "$review_script" ]; then
-            $review_script $file
+            $review_script $file | tee out/autobuilder.log
         else
             echo "cannot find $review_script" > out/fail/$ref
         fi
         rm $file
+        echo "No build is running." > tee out/autobuilder.log
     done
 done
