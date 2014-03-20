be2ec850dc1a73d314925a82be5083ba357339e3
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-03-20 16:02:26 +0800

    fetch all gerrit/* branches

--- add-head.sh
+++ add-head.sh
@@ -21,7 +21,7 @@ chmod a+w out/errcache
 
 ( cd build &&
   git remote show &&
-  git fetch gerrit )
+  git fetch gerrit refs/heads/*:refs/heads/* )
 
 if [ -z "$branch_list" ]; then
 	branch_list="$(./branches.sh)"
