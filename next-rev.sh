b7bfc81998e976b9e6ecbcb7abaa4fcd3e8e5b8d
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-03-20 14:06:55 +0800

    correctly resolve paths when build/ is a symbol link

--- next-rev.sh
+++ next-rev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR=$(dirname $0)
+DIR=`pwd`
 cd "$DIR/build"
 
 bisect()
@@ -7,13 +7,13 @@ bisect()
 	(git rev-list --first-parent --bisect-all "$@" ||
 	 git rev-list --first-parent "$@" ) |
 		while read x y; do
-			[ -e ../out/pass/$x -o -e ../out/fail/$x ] && continue
+			[ -e $DIR/out/pass/$x -o -e $DIR/out/fail/$x ] && continue
 			echo $x
 			exit 0
 		done
 }
 
-../revlist.sh "$@" | (
+$DIR/revlist.sh "$@" | (
 	pending=
 	while read commit junk; do
 		# always return the first commit in this branch
@@ -27,4 +27,3 @@ bisect()
 
 	# if we don't print anything at all, it means there's nothing to build!
 )
-
