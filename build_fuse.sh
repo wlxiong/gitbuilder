ce5bfc3edd0b0f883c6e7917f4b9b1ae8ce190ea
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-03-25 16:34:53 +0800

    add script for building fuse

---
+++ build_fuse.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+#
+# Copy this file to build.sh so that gitbuilder can run it.
+#
+# What happens is that gitbuilder will checkout the revision of your software
+# it wants to build in the directory called gitbuilder/build/.  Then it
+# does "cd build" and then "../build.sh" to run your script.
+#
+# You might want to run ./configure here, make, make test, etc.
+#
+
+# Actually build the project
+[[ -d build_fuse ]] && rm -rf build_fuse
+mkdir build_fuse
+
+git --git-dir=build/.git --work-tree=build_fuse/ checkout gerrit/dev -- fuse_client
+cd build_fuse/fuse_client && ./build_fuse.sh || exit 3
+
+exit 0
