918b0faa8f9688dd0a297e4fc6771cd5d2d3f363
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-07-18 10:12:16 +0800

    set timeout for building and running tests

--- autorun.sh
+++ autorun.sh
@@ -6,11 +6,12 @@ source env.sh
 run_script() {
     script=$1
     ref=$2
+    timeout=$3
     # clean up build/
     ./clean-dir.sh $ref
     # run build
     if [ -f "$script" ]; then
-        $script $ref | tee out/log | tee build_log
+        timeout $timeout $script $ref | tee out/log | tee build_log
     else
         echo "cannot find $script" > out/fail/$ref
     fi
@@ -24,8 +25,8 @@ while [ "$(ls -A out/pending)" ]; do
         ref=`basename $file`
         action=`cat $file | awk '{ print $4 }'`
         echo "`date +"%Y-%m-%d %T"` run HEAD $ref: $action" >> $DIR/event_log
-        [ "$action" = "build" ]  && run_script build/ci/run-build.sh $ref
-        [ "$action" = "review" ] && run_script build/ci/run-review.sh $ref
+        [ "$action" = "build" ]  && run_script build/ci/run-build.sh $ref 180
+        [ "$action" = "review" ] && run_script build/ci/run-review.sh $ref 600
         rm -f $file
     done
 done
