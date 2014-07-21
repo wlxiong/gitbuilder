20b54279d9e569ddad2840d9ba6f688b31352fbc
Xiong Yiliang <xiongyiliang@xunlei.com>
2014-07-21 12:21:50 +0800

    use kill -9

--- timeout.sh
+++ timeout.sh
@@ -4,6 +4,6 @@ timeout=`which timeout 2>/dev/null`
 if [ -f "$timeout" ]; then
     $timeout $@
 else
-    ( cmdpid=$$; (sleep $1; kill $cmdpid) & exec "${@:2}" )
+    ( cmdpid=$$; (sleep $1; kill -9 $cmdpid) & exec "${@:2}" )
     exit 0
 fi
